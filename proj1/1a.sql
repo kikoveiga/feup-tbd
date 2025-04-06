77af3c786bb1115f17daab0c22acf69aabbc551c
Catarina <up202403074@up.pt>
2025-04-06 13:06:33 +0100

    ALterações ao ambiente Z

--- proj1/1a.sql
+++ proj1/1a.sql
@@ -2,6 +2,10 @@
 from freguesias
 where freguesias.concelho = 1103 */
 
+select codigo, nome
+from freguesias
+where freguesias.concelho = 1103
+
 select freguesias.codigo, freguesias.nome
 from freguesias, concelhos
 where freguesias.concelho = concelhos.codigo and concelhos.nome = 'Azambuja'
