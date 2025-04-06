11d79592f3b722157bf813ec282bef6b98e5a813
José Francisco Veiga <veigajosefrancisco@gmail.com>
2025-04-06 02:27:44 +0100

    5

--- proj1/5.sql
+++ proj1/5.sql
@@ -3,3 +3,15 @@ from zvotacoes, zconcelhos, zfreguesias
 where zvotacoes.partido in ('PS', 'PPDPSD') and zconcelhos.distrito in (11, 15, 17) and zconcelhos.codigo = zfreguesias.concelho and zfreguesias.codigo = zvotacoes.freguesia
 group by zvotacoes.partido, zconcelhos.distrito
 order by votos desc
+
+-- b-tree indexes
+CREATE INDEX idx_zconcelhos_distrito ON zconcelhos(distrito);
+CREATE INDEX idx_zvotacoes_partido ON zvotacoes(partido);
+
+-- bitmap indexes
+CREATE BITMAP INDEX bm_zconcelhos_distrito ON zconcelhos(distrito);
+CREATE BITMAP INDEX bm_zvotacoes_partido ON zvotacoes(partido);
+
+-- drop indexes
+DROP INDEX idx_zconcelhos_distrito;
+DROP INDEX idx_zvotacoes_partido;
